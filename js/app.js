e7c9116505897e124f24eb26b72d9135c11a1991
Marcelo 3K <marcelo3k.dev@gmail.com>
2024-04-23 18:23:54 -0300

    fix: body scroll prevent when header is active

--- js/app.js
+++ js/app.js
@@ -1,8 +1,39 @@
 const hamburgerButton = document.querySelector('[data-js="header-hamburger"]')
 const headerList = document.querySelector('[data-js="header-list"]')
 
-hamburgerButton.addEventListener('click', (e) => {
-  const clickedElement = e.target.closest('[data-js="header-hamburger"]')
-  clickedElement.classList.toggle('active')
+function toggleBodyScroll(shouldPreventScroll) {
+  if (shouldPreventScroll) {
+    document.body.style.overflow = 'hidden'
+  } else {
+    document.body.style.overflow = ''
+  }
+
+}
+
+function toggleHamburger() {
+  hamburgerButton.classList.toggle('active')
   headerList.classList.toggle('active')
-})
+
+  const isHamburgerActive = hamburgerButton.classList.contains('active')
+  const isHeaderListActive = headerList.classList.contains('active')
+
+  const isMenuOpen = isHamburgerActive || isHeaderListActive
+
+  toggleBodyScroll(isMenuOpen)
+}
+
+function toggleHeaderList(event) {
+  const clickedElement = event.target
+  const isAnchorElement = clickedElement.tagName === 'A'
+  const isLIChidlrenElement = clickedElement.parentElement.tagName === 'LI'
+
+  if (isAnchorElement && isLIChidlrenElement) {
+    hamburgerButton.classList.remove('active')
+    headerList.classList.remove('active')
+
+    toggleBodyScroll(false);
+  }
+}
+
+hamburgerButton.addEventListener('click', toggleHamburger)
+headerList.addEventListener('click', toggleHeaderList)
